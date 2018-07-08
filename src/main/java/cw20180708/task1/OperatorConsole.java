2853d7fa0c2379b626f86d11838d47178f12da15
javasia <impex.ua@gmail.com>
2018-07-08 16:50:40 +0300

    tasks 08072018

--- src/main/java/cw20180708/task1/OperatorConsole.java
+++ src/main/java/cw20180708/task1/OperatorConsole.java
@@ -10,42 +10,46 @@ public class OperatorConsole {
         this.table = table;
     }
 
-    public void start (){
+    public void start() {
         Scanner sc = new Scanner(System.in);
         String command = "";
         boolean run = true;
-        while (run){
+        while (run) {
             System.out.println("Please type a command:");
             try {
                 run = processCommand(Command.valueOf(Command.class, sc.next().toUpperCase()));
-            }catch (IllegalArgumentException e){
+            } catch (IllegalArgumentException e) {
                 run = printNoSuchCommand(true);
             }
         }
     }
 
-    public boolean processCommand(Command command){
-        switch (command){
-            case EXIT: return false;
-            case PRINT: table.printTimeTable();
-            break;
-            case HELP: printHelp();
-            break;
-            default: return printNoSuchCommand(false);
+    public boolean processCommand(Command command) {
+        switch (command) {
+            case EXIT:
+                return false;
+            case PRINT:
+                table.printTimeTable();
+                break;
+            case HELP:
+                printHelp();
+                break;
+            default:
+                return printNoSuchCommand(false);
         }
         return true;
     }
 
     private void printHelp() {
         System.out.println("Available commands:");
-        for (Command cmd:Command.values()) {
+        for (Command cmd : Command.values()) {
             System.out.printf("%s \n", cmd.toString());
         }
     }
 
-    private boolean printNoSuchCommand(boolean shouldContinue){
+    private boolean printNoSuchCommand(boolean shouldContinue) {
         System.out.println("No such command!");
-        if(shouldContinue) printHelp();
+        if (shouldContinue) printHelp();
         return shouldContinue;
     }
 }
